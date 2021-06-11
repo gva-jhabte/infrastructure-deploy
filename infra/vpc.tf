77cb03a757e3095766347b028679c635ecfe72b0
Jonathan Habte <jonathan.habte@lloydsbanking.com>
2021-06-11 15:37:44 +0100

    modifying variables

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -37,7 +37,6 @@ resource "google_vpc_access_connector" "connector" {
 
 resource "google_service_networking_connection" "foobar" {
   network                 = google_compute_network.peering_network.id
-  project                 = var.project
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
 }
