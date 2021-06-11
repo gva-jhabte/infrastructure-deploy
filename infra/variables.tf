b00f7dfaf24bfc4f4494dbbdfd7a084be7697564
Jonathan Habte <jonathan.habte@lloydsbanking.com>
2021-06-11 15:30:07 +0100

    modifying variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,17 +1,14 @@
 variable "project" {
   description = "The project ID to create the resources in."
   type        = string
-  default       = "dcsgva-lab-sandbox-jonathan"
 }
 
 variable "region" {
   description = "The region to create the resources in."
   type        = string
-  default       = "europe-west4"
 }
 
 variable "instance_name" {
   description = "The name of the gcloud instance"
   type        = string
-  default     = "state-database"
 }
