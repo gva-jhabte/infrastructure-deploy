eb93a31a4a1b3b394f5e95cc0d28e00ab1dc665a
Jonathan Habte <jonathan.habte@lloydsbanking.com>
2021-06-14 10:42:57 +0100

    modifying variables

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,11 @@
 provider "google" {
   version = "~> 3.1.0"
-  region  = var.location
+  region  = var.region
   project = var.project_id
 }
 
 provider "google-beta" {
   version = "~> 3.1.0"
-  region  = var.location
+  region  = var.region
   project = var.project_id
 }
